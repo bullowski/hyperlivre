cff0a8d448402c9707acfa118d607f1326bfced7
Dream Forge <dream4ge@gmail.com>
2011-05-13 22:50:58 +0200

    views for notes

--- fuel/app/views/notes/add.php
+++ fuel/app/views/notes/add.php
@@ -1,9 +1,8 @@
-<h2>Create Concept</h2>
-<p>Please fill the form below with the Concept information.</p>
-<p> //todo add js validation</p>
+<h2>Create Note</h2>
+<p>Please enter your new note in the form below.</p>
 
 <?php echo $form->validation()->show_errors(); ?>
 <fieldset>
-<legend>Concepts details</legend>
-<?php echo $form->build('concepts/add');?>
+<legend>Note</legend>
+<?php echo $form->build('note/add');?>
 </fieldset>
--- fuel/app/views/notes/edit.php
+++ fuel/app/views/notes/edit.php
@@ -1,9 +1,9 @@
-<h2>Edit Concept</h2>
+<h2>Edit Note</h2>
 <p>Please fill the form below with the book information.</p>
 <p> //todo add js validation</p>
 
 <?php echo $form->validation()->show_errors(); ?>
 <fieldset>
 <legend>Concept details</legend>
-<?php echo $form->build('concepts/edit/'.$concept->id);?>
+<?php echo $form->build('notes/edit/'.$concept->id);?>
 </fieldset>
